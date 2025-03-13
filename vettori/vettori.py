58047fb6288530c89bafe4c835c5a7fbf0a637ed
kevingemolo <kevingemolo@gmail.com>
2025-03-13 11:29:50 +0100

    Vettori in Python.

---
+++ vettori/vettori.py
@@ -0,0 +1,24 @@
+def stampa(vettore):
+    for i in range(len(vettore)):
+        print(f"{vettore[i]}, ", end=" ")
+    print(f"")
+def somma(vettore):
+    som = 0
+    for i in range(len(vettore)):
+        som += vettore[i]
+    return som
+
+
+
+
+# Testing
+def init():
+    vettore = [1,2,3,4,5,6,7,8,9,10]
+
+    # Esercizio 1
+    stampa(vettore)
+
+    # Esercizio 2
+    print(f"La somma del vettore è {somma(vettore)}")
+
+init()
